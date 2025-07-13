700574931ef8ed772a9f3c155bec441e500b1fb0
calguts <cal.gutierrez@umontreal.ca>
2025-07-13 13:57:22 -0400

    Tp2   nom de la classe  + par default dans le package

---
+++ src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module com.example.prog2tp2 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.prog2tp2 to javafx.fxml;
+    exports com.example.prog2tp2;
+    exports;
+    opens to
+    exports;
+    opens to
+}
